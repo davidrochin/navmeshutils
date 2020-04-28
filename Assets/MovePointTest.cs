b13df6bec2600db11665de490ae7851ef99056bf
David Rochin <jdrc8@hotmail.com>
2020-04-28 12:06:59 -0600

    Initial commit

---
+++ Assets/MovePointTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DavidRochin;
+
+public class MovePointTest : MonoBehaviour {
+
+    public float Distance = 10f;
+    public int Quality = 5;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnDrawGizmos() {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(NavMeshUtils.Place(transform.position), NavMeshUtils.Place(transform.position) + transform.forward * Distance);
+
+        Gizmos.color = Color.blue;
+        Vector3[] points = NavMeshUtils.MovePosition(transform.position, transform.forward * Distance, Quality);
+        if(points.Length >= 2) {
+            for (int i = 1; i < points.Length; i++) {
+                Gizmos.DrawSphere(points[i], 0.05f);
+                Gizmos.DrawLine(points[i - 1], points[i]);
+            }
+        }
+
+    }
+}
---
+++ Assets/NavMeshUtils.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DavidRochin {
+
+    public static class NavMeshUtils {
+        
+        public static Vector3[] MovePosition(Vector3 position, Vector3 delta, int quality = 5) {
+
+            Debug.Log($"Initial magnitude = {delta.magnitude}");
+
+            position = Place(position);
+
+            List<Vector3> points = new List<Vector3>();
+            points.Add(position);
+
+            for (int i = 0; i < quality; i++) {
+
+                NavMeshHit hit;
+                if (NavMesh.Raycast(position, position + delta, out hit, NavMesh.AllAreas)) {
+                    Vector3 safeHitPosition = hit.position - delta.normalized * 0.01f;
+                    points.Add(safeHitPosition);
+
+                    Debug.Log($"[{i}] Hit Distance = {hit.distance}");
+                    Vector3 remainingDelta = delta - delta.normalized * (hit.distance - 0.01f);
+
+                    position = safeHitPosition;
+
+                    Debug.Log($"[{i}] Remaining magnitude = {remainingDelta.magnitude}");
+                    delta = CalculateSlide(remainingDelta, hit.normal);
+
+                    Debug.Log($"[{i}] delta.magnitude = {delta.magnitude}");
+                    
+                } else {
+                    points.Add(position + delta);
+                    break;
+                }
+
+            }
+            
+            return points.ToArray();
+        }
+
+        public static Vector3? Raycast(Vector3 position, Vector3 delta) {
+            position = Place(position);
+            NavMeshHit hit;
+            if (NavMesh.Raycast(position, position + delta, out hit, NavMesh.AllAreas)) {
+                return hit.position;
+            } else {
+                return null;
+            }
+        }
+
+        private static Vector3 CalculateSlide(Vector3 remainingDelta, Vector3 normal) {
+            Vector3 a = Vector3.Cross(normal, remainingDelta);
+            Vector3 b = Vector3.Cross(a, normal);
+            Vector3 d = b.normalized;
+            float dist = Vector3.Dot(d, remainingDelta);
+            return d * dist;
+        }
+
+        public static Vector3 Place(Vector3 point, float maxDistance = float.MaxValue) {
+            NavMeshHit hit;
+            bool valid = NavMesh.SamplePosition(point, out hit, maxDistance, NavMesh.AllAreas);
+            if (valid) {
+                return hit.position;
+            } else {
+                throw new System.Exception("Could not place point on Nav Mesh");
+            }
+            
+        }
+
+    }
+
+}
