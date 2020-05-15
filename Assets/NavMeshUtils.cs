b2f63fc362eae098f7fa9e018c4b524ccc3fdbe2
David Rochin <jdrc8@hotmail.com>
2020-05-14 22:28:21 -0600

    Redesigned the MovePosition method. See SlidePoint.

--- Assets/NavMeshUtils.cs
+++ Assets/NavMeshUtils.cs
@@ -8,6 +8,41 @@ namespace DavidRochin {
 
     public static class NavMeshUtils {
         
+        public static Vector3 SlidePoint(Vector3 position, Vector3 delta, out Vector3[] path, int quality = 5) {
+            //Debug.Log($"Initial magnitude = {delta.magnitude}");
+
+            List<Vector3> points = new List<Vector3>();
+            points.Add(position);
+
+            for (int i = 0; i < quality; i++) {
+
+                NavMeshHit hit;
+                if (NavMesh.Raycast(position, position + delta, out hit, NavMesh.AllAreas)) {
+                    Vector3 safeHitPosition = hit.position - delta.normalized * 0.01f;
+                    points.Add(safeHitPosition);
+
+                    //Debug.Log($"[{i}] Hit Distance = {hit.distance}");
+                    Vector3 remainingDelta = delta - delta.normalized * (hit.distance - 0.01f);
+
+                    position = safeHitPosition;
+
+                    //Debug.Log($"[{i}] Remaining magnitude = {remainingDelta.magnitude}");
+                    delta = CalculateSlide(remainingDelta, hit.normal);
+
+                    //Debug.Log($"[{i}] delta.magnitude = {delta.magnitude}");
+
+                } else {
+                    points.Add(position + delta);
+                    break;
+                }
+
+            }
+
+            path = points.ToArray();
+            return points[points.Count - 1];
+        }
+
+        [System.Obsolete]
         public static Vector3[] MovePosition(Vector3 position, Vector3 delta, int quality = 5) {
 
             //Debug.Log($"Initial magnitude = {delta.magnitude}");
@@ -62,7 +97,7 @@ namespace DavidRochin {
             return d * dist;
         }
 
-        public static Vector3 Place(Vector3 point, float maxDistance = float.MaxValue) {
+        public static Vector3 Place(Vector3 point, float maxDistance = 2f, int areaMask = NavMesh.AllAreas) {
             NavMeshHit hit;
             bool valid = NavMesh.SamplePosition(point, out hit, maxDistance, NavMesh.AllAreas);
             if (valid) {
